443c7f2db4d87f0e8e26341daf9a50b1d6a1dcda
anton.rusanov <edu.rusanov@gmail.com>
2026-03-16 15:43:31 +0100

    added necessary asset packs and session creation plugin

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerMenu.h
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MultiplayerMenu.generated.h"
+
+class UButton;
+class UMultiplayerSessionsSubsystem;
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMultiplayerMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void MultiplayerMenuSetup(
+						int32 NumberOfPublicConnections = 4,
+						FString TypeOfMatch = FString(TEXT("FreeForAll")),
+						FString LobbyPath = FString(TEXT("/Game/ThirdPerson/Maps/Lobby"))
+						);
+
+protected:
+
+	virtual bool Initialize() override;
+	virtual void NativeDestruct() override;
+
+	//Callbacks on custom Delegates in MultiplayerSessionsSubsystem
+	UFUNCTION()
+	void OnCreateSession(bool bWasSuccessful);
+
+	void OnFindSession(const TArray<FOnlineSessionSearchResult>& SessionResults, bool bWasSuccessful);
+	void OnJoinSession(EOnJoinSessionCompleteResult::Type Result);
+
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
+
+	UFUNCTION()
+	void OnStartSession(bool bWasSuccessful);
+	
+	
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* HostButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* JoinButton;
+
+	UFUNCTION()
+	void HostButtonClicked();
+
+	UFUNCTION()
+	void JoinButtonClicked();
+
+	void MenuTearDown();
+
+	UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
+
+	int32 NumPublicConnections{4};
+	FString MatchType{TEXT("FreeForAll")};
+
+	FString PathToLobby{TEXT("")};
+};
